1ba936cb07e0f2453c938e8680e1504da9a798cc
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-04-07 14:12:41 +0200

    log routing information

--- solr_channel/routing.py
+++ solr_channel/routing.py
@@ -1,5 +1,14 @@
 from django.conf.urls import url
 from solr_channel import consumers
+import logging
+import json
+
+log = logging.getLogger(__name__)
 websocket_urlpatterns = [
     url(r'^ws/rpc/solr/$', consumers.JsonRpcSolrPassthrough)
 ]
+
+for route in websocket_urlpatterns:
+    if issubclass(route.callback, consumers.JsonRpcHandlerBase):
+        log.error(f'{route.pattern}: {json.dumps(route.callback.commands.get(route.callback.__qualname__),indent=2)}')
+        pass
