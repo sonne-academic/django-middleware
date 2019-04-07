f58ca8829ba90a4e1b5b3b104217576ddc20aca6
David '-1' Schmid <david-1.schmid@uni-ulm.de>
2019-04-07 14:12:55 +0200

    linting

--- solr_channel/models.py
+++ solr_channel/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 
+
 class Graph(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     graph_str = models.TextField()
